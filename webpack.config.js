36626f4ddde8588a24b5f28de717deaebb9bfe5f
danilocolombi <41966783+danilocolombi@users.noreply.github.com>
2024-08-31 13:48:18 -0300

    Fix webpack path

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,7 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 // Webpack entry points. Mapping from resulting bundle name to the source file entry.
 const entries = {};
 
-// Loop through subfolders in the "Samples" folder and add an entry for each one
-const samplesDir = path.join(__dirname, "src/Samples");
+const samplesDir = path.join(__dirname, "src");
 fs.readdirSync(samplesDir).filter(dir => {
     if (fs.statSync(path.join(samplesDir, dir)).isDirectory()) {
         entries[dir] = "./" + path.relative(process.cwd(), path.join(samplesDir, dir, dir));
